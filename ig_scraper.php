8dfe4a6a0133934d6d39efc9c8c0c29c8ff44fcf
Berkay-Cubuk <berkaycubuk@gmail.com>
2020-03-10 09:38:54 +0300

    yay comments...

--- ig_scraper.php
+++ ig_scraper.php
@@ -5,7 +5,7 @@ Designed and created by Berkay Çubuk
 */
 
 class IG_Scraper {
-
+	//variables
 	public $json;
 	public $username;
 	public $biography;
@@ -28,18 +28,19 @@ class IG_Scraper {
 	public $picture_hd;
 	public $connected_fb_page;
 
-
 	//splitting function
 	function __construct($username) {	
-		$this->username = $username;
 		$url = "https://instagram.com/" . $username . "/?__a=1";
 
+		//get json output
 		$data = file_get_contents($url);
 
 		$this->json = $data;
 
+		//decoded json ouput
 		$data = json_decode($data);
 
+		$this->username = $username;
 		$this->biography = $data->graphql->user->biography;
 		$this->external_url = $data->graphql->user->external_url;
 		$this->external_url_linkshimmed = $data->graphql->user->external_url_linkshimmed;
@@ -54,86 +55,107 @@ class IG_Scraper {
 		$this->connected_fb_page = $data->graphql->user->connected_fb_page;
 	}
 
+	//return all json data
 	function json() {
 		return $this->json;
 	}
 
+	//return biogprahy
 	function biography() {
 		return $this->biography;
 	}
 
+	//return username
 	function username() {
 		return $this->username;
 	}
 
+	//return external_url
 	function external_url() {
 		return $this->external_url;
 	}
 
+	//return external_url_linkshimmed
 	function external_url_linkshimmed() {
 		return $this->external_url_linkshimmed;
 	}
 
+	//return followed_count
 	function followed_count() {
 		return $this->followed_count;
 	}
 
+	//return follow_count
 	function follow_count() {
 		return $this->follow_count;
 	}
 
+	//return full_name
 	function full_name() {
 		return $this->full_name;
 	}
 
+	//return has_channel
 	function has_channel() {
 		return $this->has_channel;
 	}
 
+	//return has_blocked_viewer
 	function has_blocked_viewer() {
 		return $this->has_blocked_viewer;
 	}
 
+	//return id
 	function id() {
 		return $this->id;
 	}
 
+	//return is_business_account
 	function is_business_account() {
 		return $this->is_business_account;
 	}
 
+	//return is_joined_recently
 	function is_joined_recently() {
 		return $this->is_joined_recently;
 	}
 
+	//return business_category_name
 	function business_category_name() {
 		return $this->business_category_name;
 	}
 
+	//return category_id
 	function category_id() {
 		return $this->category_id;
 	}
 
+	//return overall_category_name
 	function overall_category_name() {
 		return $this->overall_category_name;
 	}
 
+	//return is_private
 	function is_private() {
 		return $this->is_private;
 	}
 
+	//return is_verified
 	function is_verified() {
 		return $this->is_verified;
 	}
 
+	//return picture
 	function picture() {
 		return $this->picture;
 	}
 
+	//return picture_hd
 	function picture_hd() {
 		return $this->picture_hd;
 	}
 
+	//return connected_fb_page
 	function connected_fb_page() {
 		return $this->connected_fb_page;
 	}
